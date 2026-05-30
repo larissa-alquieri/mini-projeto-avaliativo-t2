6ac7dc4b817a46708bf5cda79b0e2a0aaac59f3d
larissa-alquieri <alquieri.larissa@gmail.com>
2026-05-30 19:23:27 -0300

    Limpeza

--- notebook/README_LarissaAlquieri_Turma-T2.md
+++ notebook/README_LarissaAlquieri_Turma-T2.md
@@ -1,18 +1,27 @@
-Análise de Dados com Python [T2]
-Mini-Projeto Avaliativo - Módulo 1 - Semana 07
-Estudante: Larissa Alquieri
+# **Análise de Dados com Python [T2]**
+## Mini-Projeto Avaliativo - Módulo 1 - Semana 07
+### Estudante: Larissa Alquieri
 
 
-============== DESCRIÇÃO INICIAL DO MINI-PROJETO =============
+## Descrição Inicial:
 Este Mini-Projeto tem como objetivo a Análise Exploratória de Dados aplicada ao varejo para aprender como transformar dados brutos em informações útei, para que seja possível a aplicação da parte teórica aprendida nas aulas em análises reais.
-============================*=============================
 
+## Instruções Iniciais:
+Para que seja possível a execução correta dos arquivos deste mini-projeto , é necessário ter instalado em seu computador: 
+* Python
+* Visual Studio Code [VS Code]
+* Bibliotecas: Pandas, Numpy, Matplotlib e Seaborn
 
-============== INSTRUÇÕES INICIAIS ================
-1.Para que seja possível a execução correta dos arquivos deste mini-projeto , é necessário ter instalado em seu computador: Python, Visual Studio Code [VS Code] e as bibliotecas Pandas e NumPy.
-============================*=============================
+## INSIGHTS:
 
-========== INSIGHTS ==========
-Ao primeiro comando para o levantamento de registros e colunas foi encontrado 830000 registros e apenas 1 coluna. Por isso foi necessário acrescentar o comando 'sep=' no comando inicial de leitura do arquivo para que a bilbioteca pandas conseguisse identificar a separação de colunas da maneira correta. Após este comando foram encontradas 14 colunas no arquivo.
+1. Ao primeiro comando para o levantamento de registros e colunas foi encontrado 830000 registros e apenas 1 coluna. Por isso foi necessário acrescentar o comando 'sep=' no comando inicial de leitura do arquivo para que a bilbioteca pandas conseguisse identificar a separação de colunas da maneira correta. Após este comando foram encontradas 14 colunas no arquivo
 
-Após os primeiros comandos percebeu-se que existiam 4 colunas com elementos vazios pois ao analisar a quantidade de valores nulos por coluna, elas apresentaram exatos 830.000 elementos nulos ou seja, a coluna inteira era nula.
+2. Após os primeiros comandos percebeu-se que existiam 4 colunas com elementos vazios pois ao analisar a quantidade de valores nulos por coluna, elas apresentaram exatos 830.000 elementos nulos ou seja, a coluna inteira era nula.
+
+3. Ao rodar os comandos para as médias e medianas relacionados a filhos dos clientes pode-se observar que a média de filhos é de 1.15 e a mediana é de 0. Com isso observa-se que a maioria dos clientes não possui filhos.Quando se observa o desvio padrão nota-se que este valor nos leva aos dados de que apesar de ter clientes sme filhos, existem alguns que possuem mais do que 1 filho, mostrando que existem perfis diferentes dentro de uma mesma base de clientes.
+
+*IMAGEM REPRESENTANDO OS DADOS ENCONTRADOS NA MEDIA E MEDIANA REFERENTE AOS FILHOS DE CLIENTES:*
+
+![Gráfico Demonstrando a relação clientes x filhos](C:\Users\Larissa\Python\mini-projeto\notebook\Gráfico Filhos por Cliente.png)
+
+É possível observar pelo gráfico que existe uma gama diversificada de clientes com filhos, porém a maioria não possui filhos. Estes dados podem ser importantes para influenciar as definições mais acertadas para novas campanhas ou melhorar as já existentes dos públicos alvos definidos.
--- notebook/analise-varejo.py
+++ notebook/analise-varejo.py
@@ -1,36 +1,57 @@
-#IMPORTAÇÃO DAS BIBLIOTECAS
+#IMPORTAÇÃO DAS BIBLIOTECAS:
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-#LEITURA ARQUIVO BASE
+#LEITURA ARQUIVO BASE:
 df_original = pd.read_csv('./data/original/Base Varejo.csv', sep=';') #Reconhecimento de ';' como coluna
 
-#CÓPIA DO ARQUIVO ORIGINAL PARA NÃO MODIFICAR AS INFORMAÇÕES BASE
+#CÓPIA DO ARQUIVO ORIGINAL PARA NÃO MODIFICAR AS INFORMAÇÕES BASE:
 df = df_original.copy()
 
-#RECONHECIMENTO PRIMEIRAS INFORMAÇÕES DO ARQUIVO BASE
-#print(f"Total de registros: {df.shape[0]}") #Registros
-#print(f"Total de colunas: {df.shape[1]}") #Colunas
-#print(df.columns.tolist()) #Nome das Colunas
-#print(df.head(3)) #Primeiras linhas
-#print(df.dtypes)# Tipo de cada coluna
-#print(df.isnull().sum()) # %Valores Nulos
 
-#REMOÇÃO DE COLUNAS NULAS
+#RECONHECIMENTO PRIMEIRAS INFORMAÇÕES DO ARQUIVO BASE:
+print(f"Total de registros: {df.shape[0]}") #Registros
+print(f"Total de colunas: {df.shape[1]}") #Colunas
+print(df.columns.tolist()) #Nome das Colunas
+print(df.head(3)) #Primeiras linhas
+print(df.dtypes)# Tipo de cada coluna
+print(df.isnull().sum()) # %Valores Nulos
+
+#REMOÇÃO DE COLUNAS NULAS:
 colunas_nulas = ['Unnamed: 10', 'Unnamed: 11', 'Unnamed: 12', 'Unnamed: 13']
 df.drop(columns=colunas_nulas, inplace=True)
 print("Col. Nulas ok")
 
-#DEIXANDO AS COLUNAS COM AS ESCRITAS DA MESMA MANEIRA
+#DEIXANDO AS COLUNAS COM AS ESCRITAS DA MESMA MANEIRA:
 colunas_texto = ['CL_GENERO', 'CL_SEG', 'PR_CAT', 'PR_NOME']
 for col in colunas_texto:
     df[col] = df[col].astype(str).str.strip().str.upper()
 print("Col. Textos ok")
 
+#TRANSFORMAÇÃO DO TIPO DAS COLUNAS:
 colunas_inteiras = ['CO_ID', 'CL_ID', 'CL_EC', 'CL_FHL', 'PR_ID']
 for col in colunas_inteiras:
     df[col] = df[col].astype('int64')
 print("Col.Inteiras ok")
+
+#REMOÇÃO DE DUPLICADOS:
+duplicados_qtd = df.duplicated().sum()
+if duplicados_qtd > 0:
+    df.drop_duplicates(inplace=True)
+print("Duplicados ok")
+
+#REMOÇÃO DE NULOS:
+df.dropna(inplace=True)
+print("Nulos ok")
+
+#TRANSFORMAÇÃO DA COLUNA DATA EM FORMATO ADEQUADO:
+df['DATA'] = pd.to_datetime(df['DATA'], format='%d/%m/%Y', errors='coerce')
+print("Data ok")
+
+#GERANDO CÓPIA ARQUIVO BASE:
+df.to_csv('./data/arquivo.limpo/Varejo_Limpo.csv', sep=';', index=False)
 
 #REMOÇÃO DE DUPLICADOS
 duplicados_qtd = df.duplicated().sum()
@@ -42,11 +63,46 @@ print("Duplicados ok")
 df['DATA'] = pd.to_datetime(df['DATA'], format='%d/%m/%Y', errors='coerce')
 print("Data ok")
 
+#ESTATÍSTICAS:
 
+#Número de Filhos por cliente
+coluna_filhos = 'CL_FHL'
+estatisticas = df[coluna_filhos].describe()
+quantidade_padrao_filhos = df[coluna_filhos].mode()[0]
+print(f"Média de filhos:  {estatisticas['mean']:.2f}")
+print(f"Mediana de filhos: {estatisticas['50%']:.2f}")
+print(f"Mode de filhos:    {quantidade_padrao_filhos}")
+print(f"Desvio Padrão:     {estatisticas['std']:.2f}\n")
 
+#CRIAÇÃO DE GRÁFICO PARA REPRESENTAÇÃO DAS ESTATISTICAS ENCONTRADAS CL_FHL:
 
+dados_filhos = df["CL_FHL"].values
+fig, ax = plt.subplots(figsize=(9, 5))
+ax.set_facecolor('white')
+sns.histplot(data=df, x='CL_FHL', discrete=True, hue='CL_FHL',
+              palette='dark',alpha=0.85, ax=ax, legend=False,
+              edgecolor='white',linewidth=2)
 
+media_filhos = estatisticas ["mean"]
+mediana_filhos = estatisticas ["50%"]
 
+ax.axvline(media_filhos,   color="#0A0A0A",
+            linewidth=2, linestyle='--', label=f'Média: {media_filhos:.2f}')
 
-#GERANDO ARQUIVO NOVO DE LIMPEZA
-df.to_csv('./data/arquivo.limpo/Varejo_Limpo.csv', sep=';', index=False)
+ax.axvline(mediana_filhos, color='orange',
+            linewidth=2, linestyle='--',  label=f'Mediana: {mediana_filhos:.2f}')
+
+ax.set_title("FILHOS POR CLIENTE", fontsize=10, fontweight='bold')
+ax.set_xlabel("Quantidade de Filhos (CL_FHL)", fontsize=9)
+ax.set_ylabel("Quantidade de Clientes", fontsize=10)
+ax.legend(fontsize=9)
+
+ax.grid(axis='y', linestyle=':', alpha=0.3)
+ax.set_axisbelow(True)
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['left'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
+plt.tight_layout()
+plt.show()
+

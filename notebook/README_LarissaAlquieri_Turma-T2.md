6164972638a64f6f0ef662114f7a45233d15453c
larissa-alquieri <alquieri.larissa@gmail.com>
2026-06-01 22:20:03 -0300

    Atualizações Finais e Conclusão

--- notebook/README_LarissaAlquieri_Turma-T2.md
+++ notebook/README_LarissaAlquieri_Turma-T2.md
@@ -3,25 +3,41 @@
 ### Estudante: Larissa Alquieri
 
 
-## Descrição Inicial:
+## Descrição Inicial
 Este Mini-Projeto tem como objetivo a Análise Exploratória de Dados aplicada ao varejo para aprender como transformar dados brutos em informações útei, para que seja possível a aplicação da parte teórica aprendida nas aulas em análises reais.
 
-## Instruções Iniciais:
+## Instruções Iniciais
 Para que seja possível a execução correta dos arquivos deste mini-projeto , é necessário ter instalado em seu computador: 
 * Python
 * Visual Studio Code [VS Code]
 * Bibliotecas: Pandas, Numpy, Matplotlib e Seaborn
 
-## INSIGHTS:
+## ETL
+A extração, transformação e carregamento de dados é base para uma análises de dados coerente.Neste mini-projeto utilizamos destes recursos para gerar análises coerentes e de fácil entendimento, pois é nesta etapa que são encontradas as inconsistências que prejudicariam os insights e as conclusões.
+Tratar destes dados é de suma importância para não haver distorção nas estatísticas levantadas e causar falsas afirmações sobre os dados encontrados.
 
-1. Ao primeiro comando para o levantamento de registros e colunas foi encontrado 830000 registros e apenas 1 coluna. Por isso foi necessário acrescentar o comando 'sep=' no comando inicial de leitura do arquivo para que a bilbioteca pandas conseguisse identificar a separação de colunas da maneira correta. Após este comando foram encontradas 14 colunas no arquivo
 
-2. Após os primeiros comandos percebeu-se que existiam 4 colunas com elementos vazios pois ao analisar a quantidade de valores nulos por coluna, elas apresentaram exatos 830.000 elementos nulos ou seja, a coluna inteira era nula.
+## Preparação dos Dados
+Antes de serem realizadas as análises exploratórias junto ao banco de dados, foram realizadas algumas limpezas para melhora na hora de serem realizadas as análises. Dentre os processos realizados cabe destacar a eliminação de 4 colunas que não incluiam dados, foram removidos espaços desnecessários entre as palavras, remoção de linhas duplicadas, eliminação de elementos nulos que atrapalhariam a estatística, conversão de datas para que fosse possível uma melhor análise. O arquivo tratado foi salvo na pasta `./data/arquivo.limpo/Varejo_Limpo.csv` para versionamento.
 
-3. Ao rodar os comandos para as médias e medianas relacionados a filhos dos clientes pode-se observar que a média de filhos é de 1.15 e a mediana é de 0. Com isso observa-se que a maioria dos clientes não possui filhos.Quando se observa o desvio padrão nota-se que este valor nos leva aos dados de que apesar de ter clientes sme filhos, existem alguns que possuem mais do que 1 filho, mostrando que existem perfis diferentes dentro de uma mesma base de clientes.
+## INSIGHTS
 
-*IMAGEM REPRESENTANDO OS DADOS ENCONTRADOS NA MEDIA E MEDIANA REFERENTE AOS FILHOS DE CLIENTES:*
+### Ajustes nos registros
+Ao primeiro comando para o levantamento de registros e colunas foi encontrado 830000 registros e apenas 1 coluna. Por isso foi necessário acrescentar o comando 'sep=' no comando inicial de leitura do arquivo para que a bilbioteca pandas conseguisse identificar a separação de colunas da maneira correta. Após este comando foram encontradas 14 colunas no arquivo. Após os primeiros comandos percebeu-se que existiam 4 colunas com elementos vazios pois ao analisar a quantidade de valores nulos por coluna, elas apresentaram exatos 830.000 elementos nulos ou seja, a coluna inteira era nula.
 
-![Gráfico Demonstrando a relação clientes x filhos](C:\Users\Larissa\Python\mini-projeto\notebook\Gráfico Filhos por Cliente.png)
+### Média e mediana
+Ao rodar os comandos para as médias e medianas relacionados a filhos dos clientes pode-se observar que a média de filhos é de 1.15** e a **mediana é de 0. 
+Com isso observa-se que a **maioria dos clientes não possui filhos**.Quando se observa o desvio padrão nota-se que este valor nos leva aos dados de que apesar de ter clientes sem filhos, existem alguns que possuem mais do que um filho, mostrando perfis diferentes dentro de uma mesma base de clientes. Ao gerar o **'Gráfico 1'**, nota-se a  diversidade no quesito filhos, mas ainda assim prevalecendo a maioria não possuindo filhos. Estes dados podem ser importantes para influenciar as definições mais acertadas para novas campanhas ou melhorar as já existentes dos públicos alvos definidos.
 
-É possível observar pelo gráfico que existe uma gama diversificada de clientes com filhos, porém a maioria não possui filhos. Estes dados podem ser importantes para influenciar as definições mais acertadas para novas campanhas ou melhorar as já existentes dos públicos alvos definidos.
+### Volume de vendas
+Quando se faz o cruzamento de dados entre **gênero e volume de compras** e é gerado o **'Gráfico** 2', nota-se que a categoria alimentos é a mais vendida, seguido de higiene e limpeza. Outro dado que destaca-se é de que predominantemente o sexo feminino é o maior comprador, em todas as categorias.
+Analisando outro cruzamento de dados seguindo a direção das análises já feitas nos mostram que a grande maioria de mulheres**(53,35%)** não possuem filhos, seguido por mulheres que possuem apenas 1 filho**(13.1%)**. 
+
+### Produtos mais vendidos
+Quando feito a análise dos dados cruzando as informações de mulheres e alimentos, foi encontrado os produtos alimentícios com maiores vendas, conforme Gráfico 3. Dos top 5 produtos, 4 são produtos refrigerados e pode gerar sugestões de campanhas para impulsionar produtos que sejam corelacionados.
+
+## CONCLUSÃO
+Com base nas análises realizadas concluiu-se que o público feminino em todas as categorias são a maioria nas vendas, o gênero masculino não teve o maior foco nestas análises haja vista que a predominância nas vendas é do público feminino.
+Com isso se fez necessário uma maior atenção nas escolhas das análises, aumentando o foco no perfil de consumo feminino e consequentemente auxiliando nas escolhas de campanhas e posicionamento de produtos.
+ Os produtos com maior destaque dentro da categoria alimentícia foram os refrigerados,dentre os  principais com maior volume de vendas refrigerados e de uso diário, onde de cinco itens quatro são desta categoria.
+O posicionamento de produtos é crucial para trazer maior visibilidade e assim possivelmente aumento de vendas haja vista que nas áreas onde são as maiores vendas também há consequentemente maior circulação.
--- notebook/analise-varejo.py
+++ notebook/analise-varejo.py
@@ -3,69 +3,75 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from datetime import datetime
+
+#====================================================================================
 
 #LEITURA ARQUIVO BASE:
 df_original = pd.read_csv('./data/original/Base Varejo.csv', sep=';') #Reconhecimento de ';' como coluna
 
+#==================================================================================
+
 #CÓPIA DO ARQUIVO ORIGINAL PARA NÃO MODIFICAR AS INFORMAÇÕES BASE:
 df = df_original.copy()
 
-
-#RECONHECIMENTO PRIMEIRAS INFORMAÇÕES DO ARQUIVO BASE:
-print(f"Total de registros: {df.shape[0]}") #Registros
-print(f"Total de colunas: {df.shape[1]}") #Colunas
-print(df.columns.tolist()) #Nome das Colunas
-print(df.head(3)) #Primeiras linhas
-print(df.dtypes)# Tipo de cada coluna
-print(df.isnull().sum()) # %Valores Nulos
+#================================================================================
 
 #REMOÇÃO DE COLUNAS NULAS:
 colunas_nulas = ['Unnamed: 10', 'Unnamed: 11', 'Unnamed: 12', 'Unnamed: 13']
 df.drop(columns=colunas_nulas, inplace=True)
-print("Col. Nulas ok")
+
+#=====================================================================
 
 #DEIXANDO AS COLUNAS COM AS ESCRITAS DA MESMA MANEIRA:
 colunas_texto = ['CL_GENERO', 'CL_SEG', 'PR_CAT', 'PR_NOME']
 for col in colunas_texto:
     df[col] = df[col].astype(str).str.strip().str.upper()
-print("Col. Textos ok")
 
-#TRANSFORMAÇÃO DO TIPO DAS COLUNAS:
-colunas_inteiras = ['CO_ID', 'CL_ID', 'CL_EC', 'CL_FHL', 'PR_ID']
-for col in colunas_inteiras:
-    df[col] = df[col].astype('int64')
-print("Col.Inteiras ok")
+#=================================================================================
+#TRATAMENTO NULOS
+
+df['PR_CAT'] = df['PR_CAT'].apply(lambda x: 'SEM CATEGORIA' if x in ['NAN', '', 'N/D', 'N/A'] else x)
+#=============================================================================== 
 
 #REMOÇÃO DE DUPLICADOS:
 duplicados_qtd = df.duplicated().sum()
 if duplicados_qtd > 0:
     df.drop_duplicates(inplace=True)
-print("Duplicados ok")
 
+#===============================================================================
 #REMOÇÃO DE NULOS:
-df.dropna(inplace=True)
-print("Nulos ok")
+df.dropna(subset=['CL_GENERO', 'CL_ID', 'CO_ID'], inplace=True)
 
-#TRANSFORMAÇÃO DA COLUNA DATA EM FORMATO ADEQUADO:
-df['DATA'] = pd.to_datetime(df['DATA'], format='%d/%m/%Y', errors='coerce')
-print("Data ok")
+#=================================================================================
+#DATE TIME
+
+df = df[df['CO_ID'].notnull()]
+def valida_converte_data(data_str):
+    try:
+        return datetime.strptime(str(data_str).strip(), '%d/%m/%Y')
+    except ValueError:
+        return None
+
+df['DATA'] = df['DATA'].apply(valida_converte_data)
+df.dropna(subset=['DATA'], inplace=True)
+
+#=================================================================================
+
+#TRANSFORMAÇÃO DO TIPO DAS COLUNAS:
+colunas_inteiras = ['CO_ID', 'CL_ID', 'CL_EC', 'CL_FHL', 'PR_ID']
+for col in colunas_inteiras:
+    df[col] = df[col].astype('int64')
+print("Colunas Inteiras ok")
+
+#================================================================================
 
 #GERANDO CÓPIA ARQUIVO BASE:
 df.to_csv('./data/arquivo.limpo/Varejo_Limpo.csv', sep=';', index=False)
 
-#REMOÇÃO DE DUPLICADOS
-duplicados_qtd = df.duplicated().sum()
-if duplicados_qtd > 0:
-    df.drop_duplicates(inplace=True)
-print("Duplicados ok")
+#==============================================================================
 
-#TRANSFORMAÇÃO DA COLUNA DATA EM FORMATO ADEQUADO DE DATA
-df['DATA'] = pd.to_datetime(df['DATA'], format='%d/%m/%Y', errors='coerce')
-print("Data ok")
-
-#ESTATÍSTICAS:
-
-#Número de Filhos por cliente
+#NÚMERO DE FILHOS POR CLIENTES:
 coluna_filhos = 'CL_FHL'
 estatisticas = df[coluna_filhos].describe()
 quantidade_padrao_filhos = df[coluna_filhos].mode()[0]
@@ -74,8 +80,9 @@ print(f"Mediana de filhos: {estatisticas['50%']:.2f}")
 print(f"Mode de filhos:    {quantidade_padrao_filhos}")
 print(f"Desvio Padrão:     {estatisticas['std']:.2f}\n")
 
-#CRIAÇÃO DE GRÁFICO PARA REPRESENTAÇÃO DAS ESTATISTICAS ENCONTRADAS CL_FHL:
+#=========================================================================================
 
+#GRÁFICO 1
 dados_filhos = df["CL_FHL"].values
 fig, ax = plt.subplots(figsize=(9, 5))
 ax.set_facecolor('white')
@@ -92,7 +99,7 @@ ax.axvline(media_filhos,   color="#0A0A0A",
 ax.axvline(mediana_filhos, color='orange',
             linewidth=2, linestyle='--',  label=f'Mediana: {mediana_filhos:.2f}')
 
-ax.set_title("FILHOS POR CLIENTE", fontsize=10, fontweight='bold')
+ax.set_title("Gráfico 1: FILHOS POR CLIENTE", fontsize=10, fontweight='bold')
 ax.set_xlabel("Quantidade de Filhos (CL_FHL)", fontsize=9)
 ax.set_ylabel("Quantidade de Clientes", fontsize=10)
 ax.legend(fontsize=9)
@@ -106,3 +113,71 @@ ax.spines['bottom'].set_visible(False)
 plt.tight_layout()
 plt.show()
 
+#========================================================================================
+#GRÁFICO 2
+
+df['PR_CAT'] = df['PR_CAT'].astype(str).str.strip().str.upper()
+filtro_sujeira = df['PR_CAT'].str.contains('N/D|N/A|NAN', na=True)
+df_limpo_cat = df[~filtro_sujeira]
+genero_cat = df_limpo_cat.groupby(['CL_GENERO', 'PR_CAT']).size().reset_index(name='TOTAL.VENDAS')
+print("\nAGRUPAMENTO GENEROXCATEGORIA")
+print(genero_cat.head(10))
+df_plot = df_limpo_cat.groupby(['PR_CAT', 'CL_GENERO']).size().unstack(fill_value=0)
+fig, ax = plt.subplots(figsize=(12, 6))
+ax.set_facecolor('white')
+cores = ["#a6163f", "#124fb0"]
+df_plot.plot(kind='bar', ax=ax, color=cores, width=0.8)
+for container in ax.containers:
+    ax.bar_label(container, padding=0, fontsize=7, fmt='%d', color="#5a5758")
+ax.set_title("Gráfico 2: COMPRAS CATEGORIA X GÊNERO", fontsize=12, fontweight='bold', pad=20)
+ax.set_xlabel("CATEOGRIA(PR_CAT)", fontsize=8, fontweight='bold', labelpad=10, )
+ax.set_ylabel("COMPRAS(CL_GENERO)", fontsize=8, fontweight='bold', labelpad=10)
+plt.xticks(rotation=0, ha='center')
+ax.grid(axis='y', linestyle='--', alpha=0.3)
+ax.set_axisbelow(True)
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['left'].set_color('#cccccc')
+ax.spines['bottom'].set_color('#cccccc')
+plt.tight_layout()
+plt.show()
+
+#==================================================================================================
+
+#GÊNERO FEMININO X FILHOS
+df_m = df[df['CL_GENERO'] == 'F']
+mulheres_filhos = df_m.groupby('CL_FHL').size().reset_index(name='TOTAL')
+print("\nTabela de distribuição de filhos (Público Feminino):")
+print(mulheres_filhos)
+
+mulheres_filhos['PERCENTUAL'] = (mulheres_filhos['TOTAL'] / mulheres_filhos['TOTAL'].sum()) * 100
+print("\nTabela de distribuição com Porcentagem:")
+print(mulheres_filhos)
+
+#==================================================================================================
+
+#PRODUTOS MAIS VENDIDOS ENTRE MULHERES:
+
+df_alimentos_mulheres = df_limpo_cat[(df_limpo_cat['PR_CAT'] == 'ALIMENTOS') & (df_limpo_cat['CL_GENERO'] == 'F')]
+top_produtos = df_alimentos_mulheres['PR_NOME'].value_counts().head(5)
+print(df_alimentos_mulheres)
+
+#GRÁFICO 3
+
+fig, ax = plt.subplots(figsize=(10, 5))
+ax.set_facecolor('white')
+top_produtos.plot(kind='barh', ax=ax, color="#6f0416", width=0.7)
+ax.invert_yaxis() 
+for container in ax.containers:
+    ax.bar_label(container, padding=5, fontsize=8, fmt='%d', color="#5a5758")
+ax.set_title("Gráfico 3:PRODUTOS MAIS VENDIDOS", fontsize=12, fontweight='bold', pad=20)
+ax.set_xlabel("Quantidade de Vendas (Volume)", fontsize=9, fontweight='bold', labelpad=10)
+ax.set_ylabel("Nome do Produto (PR_NOME)", fontsize=9, fontweight='bold', labelpad=10)
+ax.grid(axis='x', linestyle='--', alpha=0.3)
+ax.set_axisbelow(True)
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['left'].set_color('#cccccc')
+ax.spines['bottom'].set_color('#cccccc')
+plt.tight_layout()
+plt.show()
